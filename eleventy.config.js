c44bd078cb293498f7b69434e27b633d984b8fac
Benjamin Howe <ben@beh.uk>
2024-07-12 20:23:47 +0100

    Minify HTML

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,10 +1,36 @@
 import browserslist from "browserslist";
+import htmlmin from "html-minifier-terser";
 import { bundle, browserslistToTargets } from "lightningcss";
 import path from "path";
 
 let targets = browserslistToTargets(browserslist("> 0.2% and not dead"));
 
 export default async function (eleventyConfig) {
+  eleventyConfig.addTransform("htmlmin", function (content) {
+		if ((this.page.outputPath || "").endsWith(".html")) {
+			let minified = htmlmin.minify(content, {
+        collapseBooleanAttributes: true,
+        collapseInlineTagWhitespace: true,
+				collapseWhitespace: true,
+        decodeEntities: true,
+        minifyCSS: true,
+        minifyJS: true,
+        minifyURLs: true,
+        removeAttributeQuotes: true,
+				removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        sortAttributes: true,
+        sortClassName: true,
+				useShortDoctype: true,
+			});
+			return minified;
+		}
+		// if not HTML, return content as-is
+		return content;
+	});
+
   // css processing inspired by https://11ty.rocks/posts/process-css-with-lightningcss/
   eleventyConfig.addTemplateFormats("css");
   eleventyConfig.addExtension("css", {
