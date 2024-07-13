678bdce7e221b7f5c073325d0f7df30f311048b1
Benjamin Howe <ben@beh.uk>
2024-07-13 18:20:26 +0100

    Refactor main worker

--- functions/api/contact.js
+++ functions/api/contact.js
@@ -9,26 +9,20 @@ export async function onRequest(context) {
   const fields = Object.fromEntries(formData.entries());
   fields.cf = context.request.cf;
   fields.headers = Object.fromEntries(context.request.headers.entries());
-  if (fields[HONEYPOT_FIELD] !== "") {
-    return Response.redirect(context.request.headers.get("Referer"), 303);
-  } else {
+  if (fields[HONEYPOT_FIELD] === "") {
     delete fields[HONEYPOT_FIELD];
+    const sent = await sendFormViaResend({
+      apiKey: context.env.RESEND_KEY,
+      emailFrom: context.env.EMAIL_FROM,
+      emailTo: context.env.EMAIL_TO,
+      fields,
+      subject: `Form submission from ${new URL(context.request.url).hostname}`,
+    });
+
+    if (!sent) {
+      return new Response("Oops! Something went wrong. Please try submitting the form again.", { status: 500 });
+    }
   }
-
-  const url = new URL(context.request.url); 
-
-  const sent = await sendFormViaResend({
-    apiKey: context.env.RESEND_KEY,
-    emailFrom: context.env.EMAIL_FROM,
-    emailTo: context.env.EMAIL_TO,
-    fields,
-    subject: `Form submission from ${url.hostname}`,
-});
-
-  if (!sent) {
-    return new Response("Oops! Something went wrong. Please try submitting the form again.", { status: 500 });
-  }
-
   return Response.redirect(context.request.headers.get("Referer"), 303);
 }
 
